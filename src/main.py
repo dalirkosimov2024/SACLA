591e92728e2ea8713a67bbb62499a80a8ef6bbd4
dalir <lcv510@york.ac.uk>
2025-02-18 13:37:32 +0000

    first edition of the code - main.py converts tiff file to png

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import tifffile as tiff
+import matplotlib.pyplot as plt 
+
+path = "/home/dalir/Documents/cdt/japan/script/docs"
+name = "1478724-02-shot"
+
+def tif_runner(path, name):
+
+    shot_id = name.split("-")[0]
+    shot_number = name.split("-")[1]
+    state = name.split("-")[2]
+
+    if state == "ref":
+        state = "BEFORE"
+    elif state == "pref":
+        state = "AFTER"
+    elif state == "shot":
+        state = "SHOT"
+
+    print(shot_id)
+    image = tiff.imread(f"{path}/{name}.tif")
+
+    plt.imshow(image, cmap="plasma")
+    plt.colorbar()
+    plt.suptitle(f"|  {state}  |  SHOT NUMBER: {shot_number}  |  SHOT ID: {shot_id}  |")
+    plt.show()
+   
+
+if __name__ == "__main__": 
+    tif_runner(path, name)
