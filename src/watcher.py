fadc7e6862d847f4bdcf33de15b0ebadd742300c
dalir <lcv510@york.ac.uk>
2025-02-20 20:43:19 +0000

    added function that monitors a dir and copies anything that is modified there into another dir

--- src/watcher.py
+++ src/watcher.py
@@ -3,11 +3,26 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 import logging
+import shutil
+import os
+
+# Monitors 'path', anything that is added there is copied to 'new_path'
+def on_modified(event):
+    new_path = f'{os.getcwd()}/NEW_PATH'
+    for file_name in os.listdir(path):
+        source = f'{path}/{file_name}'
+        sink = f'{new_path}/{file_name}'
+        
+        if os.path.isfile(source):
+            shutil.copy(source, sink)
+            print(f"Copied {file_name}")
+
 
 if __name__ == "__main__":
 
-    # log the changes in terminal
-    logging.basicConfig(level=logging.INFO, 
+    # log the changes in terminal, save changes to dev.log file
+    logging.basicConfig(filename = 'dev.log', filemode = 'a',
+                        level=logging.INFO, 
                         format = '%(asctime)s -%(process)d - %(message)s',
                         datefmt = '%Y - %m - %d %H:%M:%S')
 
@@ -16,6 +31,7 @@ if __name__ == "__main__":
     # watch for changes in path
     watcher = Observer()
     handler = LoggingEventHandler()
+    handler.on_modified = on_modified
     watcher.schedule(handler, path, recursive = True)
     watcher.start()
 
