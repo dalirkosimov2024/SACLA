2cf54b7fc33146aa5d092f8dd8dc41fa0412c6df
dalir <lcv510@york.ac.uk>
2025-02-26 15:47:41 +0000

    Added two functions that connect to SFTP server in SACLA and copy files from the .tif directory into a local directory

--- src/watcher.py
+++ src/watcher.py
@@ -5,9 +5,14 @@ from watchdog.events import LoggingEventHandler
 import logging
 import shutil
 import os
+import paramiko
+from PIL import Image
+from io import BytesIO
+import numpy as np
 
 # Monitors 'path', anything that is added there is copied to 'new_path'
 def on_modified(event):
+
     new_path = f'{os.getcwd()}/NEW_PATH'
     for file_name in os.listdir(path):
         source = f'{path}/{file_name}'
@@ -17,8 +22,8 @@ def on_modified(event):
             shutil.copy(source, sink)
             print(f"Copied {file_name}")
 
-
-if __name__ == "__main__":
+def watcher():
+    print("Operation: watcher")
 
     # log the changes in terminal, save changes to dev.log file
     logging.basicConfig(filename = 'dev.log', filemode = 'a',
@@ -26,7 +31,6 @@ if __name__ == "__main__":
                         format = '%(asctime)s -%(process)d - %(message)s',
                         datefmt = '%Y - %m - %d %H:%M:%S')
 
-    path = f'{os.getcwd()}/PATH'
 
     # watch for changes in path
     watcher = Observer()
@@ -42,4 +46,61 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         watcher.stop()
         watcher.join()
+
+def connect_to_sftp():
+
+    hostname ='xhpcfep.hpc.spring8.or.jp' 
+    username = 'dkosimov'
+    password = 'mUYquZc8'
+    port = 22
+
+    SSH_Client = paramiko.SSHClient()
+    SSH_Client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    SSH_Client.connect(hostname=hostname,
+                       port=port,
+                       username=username,
+                       password=password,
+                       look_for_keys=False)
+
+    sftp_client = SSH_Client.open_sftp()
+    print("Connection successfully established ... ")
+    return sftp_client
+
+def copy_from_sftp(sftp_client):
+
+
+    tif_path = '/work/kmiyanishi/userdata/2024b/woolsey2024b/difras'
+    new_path = f'{os.getcwd()}/NEW_PATH'
+
+    file_name = '1479042_kame.tif'
+    print("Fetching file ...")
+
+    try:
+      sftp_client.get(f'{tif_path}/{file_name}',f'{new_path}/{file_name}')
+      print("File succesfully downloaded.")
+    except FileNotFoundError as err:
+      print(f"File: {file_name} was not found on the SFTP server")
+
+
+       
+
+
+
+
+if __name__ == "__main__":
+
+    path = f'{os.getcwd()}/PATH'
+    operation = 'stfp'
+
+    if operation == 'watcher':
+
+        print("Operation: watcher")
+        watcher()
+
+    elif operation == 'sftp':
+        
+        print("Operation: SFTP")
+        sftp_client = connect_to_sftp()
+        copy_from_sftp(sftp_client)
+
    
