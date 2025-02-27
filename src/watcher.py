d6218a40a387bb0b19c92526cdc29b2203f51bf2
dalir <lcv510@york.ac.uk>
2025-02-27 08:32:16 +0000

    made my own watcher, as watchdog cannot work on sftp servers

--- src/watcher.py
+++ src/watcher.py
@@ -1,51 +1,27 @@
-import sys
 import time
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
-import logging
-import shutil
 import os
 import paramiko
-from PIL import Image
-from io import BytesIO
-import numpy as np
 
 # Monitors 'path', anything that is added there is copied to 'new_path'
-def on_modified(event):
-
-    new_path = f'{os.getcwd()}/NEW_PATH'
-    for file_name in os.listdir(path):
-        source = f'{path}/{file_name}'
-        sink = f'{new_path}/{file_name}'
-        
-        if os.path.isfile(source):
-            shutil.copy(source, sink)
-            print(f"Copied {file_name}")
-
-def watcher():
-    print("Operation: watcher")
-
-    # log the changes in terminal, save changes to dev.log file
-    logging.basicConfig(filename = 'dev.log', filemode = 'a',
-                        level=logging.INFO, 
-                        format = '%(asctime)s -%(process)d - %(message)s',
-                        datefmt = '%Y - %m - %d %H:%M:%S')
 
 
-    # watch for changes in path
-    watcher = Observer()
-    handler = LoggingEventHandler()
-    handler.on_modified = on_modified
-    watcher.schedule(handler, path, recursive = True)
-    watcher.start()
+def watcher(sftp):
 
-    # stop script if I use keyboard
+    source = sftp.listdir(tif_path)
+
+    existing_files = set(source)
+    print(f'Existing files: {existing_files}')
     try:
         while True:
+            current_files = set(source)
+            new_files = current_files - existing_files
+            for file in new_files:
+                copy_from_sftp(sftp, file)
+
+            existing_files = current_files
             time.sleep(1)
     except KeyboardInterrupt:
-        watcher.stop()
-        watcher.join()
+        print("\n Stopping...")
 
 def connect_to_sftp():
 
@@ -62,17 +38,13 @@ def connect_to_sftp():
                        password=password,
                        look_for_keys=False)
 
-    sftp_client = SSH_Client.open_sftp()
+    sftp = SSH_Client.open_sftp()
     print("Connection successfully established ... ")
-    return sftp_client
+    return sftp
 
-def copy_from_sftp(sftp_client):
+def copy_from_sftp(sftp, file):
 
-
-    tif_path = '/work/kmiyanishi/userdata/2024b/woolsey2024b/difras'
-    new_path = f'{os.getcwd()}/NEW_PATH'
-
-    file_name = '1479042_kame.tif'
+    file_name = file
     print("Fetching file ...")
 
     try:
@@ -82,25 +54,17 @@ def copy_from_sftp(sftp_client):
       print(f"File: {file_name} was not found on the SFTP server")
 
 
-       
-
-
-
-
 if __name__ == "__main__":
 
+    tif_path = '/work/kmiyanishi/userdata/2024b/woolsey2024b/difras'
+    new_path = f'{os.getcwd()}/NEW_PATH'
     path = f'{os.getcwd()}/PATH'
-    operation = 'stfp'
 
-    if operation == 'watcher':
 
-        print("Operation: watcher")
-        watcher()
+    sftp = connect_to_sftp()
+    watcher(sftp)
 
-    elif operation == 'sftp':
-        
-        print("Operation: SFTP")
-        sftp_client = connect_to_sftp()
-        copy_from_sftp(sftp_client)
+
+
 
    
